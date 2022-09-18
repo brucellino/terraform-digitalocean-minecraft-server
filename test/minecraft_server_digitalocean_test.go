6a643a707c224dad00355cfd5684b8f8c17230ec
Bruce Becker <brucellino@protonmail.com>
2022-09-18 18:31:03 +0200

    test: implement passing test
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- test/minecraft_server_digitalocean_test.go
+++ test/minecraft_server_digitalocean_test.go
@@ -6,9 +6,13 @@ import "github.com/gruntwork-io/terratest/modules/terraform"
 func TestMinecraftServerDigitalocean(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../examples/simple",
+		// EnvVars: {
+		// 	"CONSUL_HTTP_ADDR": "http://bare.station:8500"
+		// },
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
 
 	terraform.InitAndValidate(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
 }
